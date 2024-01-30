1f2a001c883ae4cbbbcf9e709d37a2b9436e204f
Marcos Ruiz Rubio <marcosruizrubio@gmail.com>
2024-01-30 12:07:12 +0100

    Agregado fichero con función para conectarse a la BD

---
+++ public/iniciarPDO.php
@@ -0,0 +1,13 @@
+<?php
+function iniciarPDO(): PDO {
+    $env = parse_ini_file('.env');
+    $host = $env["IP"];
+    $dbname = $env["DATABASE"];
+    $user = $env["USUARIO"];
+    $pass = $env["PASSWORD"];
+
+    //Inicia la conexion a la DB
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    return $pdo;
+}
+?>
