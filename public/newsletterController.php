ab883b9cc47b0be01a0141d227df984b68ca0913
Marcos150 <marcosruizrubio@gmail.com>
2024-02-08 13:16:23 +0100

    Implementado funcionamiento de suscripción al newsletter

--- public/newsletterController.php
+++ public/newsletterController.php
@@ -2,20 +2,28 @@
 require_once("iniciarPDO.php");
 
 if (isset($_POST["email"]) && filter_var($_POST["email"], FILTER_SANITIZE_EMAIL) && filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
-    $pdo = iniciarPDO();
-    $pdo->query("SELECT * FROM usuarios u WHERE u.suscritoNewsletter = FALSE");
-
-    foreach ($conn->query($sql) as $row) {
-        if ($row["correo"] == $_POST["email"]){
-            //Ya esta suscrito, se le indica un error
-            header("location: index.html?error=yaSuscrito");
-            die;
+    try {
+        $pdo = iniciarPDO();
+        $resultado = $pdo->query("SELECT * FROM usuarios u WHERE u.suscritoNewsletter = TRUE");
+        
+        foreach ($resultado as $row) {
+            echo "a";
+            if ($row["correo"] == $_POST["email"]){
+                //Ya esta suscrito, se le indica un error
+                header("location: index.html?suscrito=false&error=yaSuscrito");
+                die;
+            }
         }
+        $pdo->exec("UPDATE usuarios SET suscritoNewsletter=TRUE WHERE correo = '{$_POST["email"]}';");
+        header("location: index.html?suscrito=true");
+        die;
+    } catch(Exception $e) {
+        echo $e;
+        header("location: index.html?suscrito=false&error=errorBaseDatos");
+        die;
     }
-
-    $pdo->exec("UPDATE usuarios u SET u.suscritoNewsletter=TRUE WHERE u.correo = {$_POST["email"]}");
 }
 
-header("location: index.html");
+header("location: index.html?suscrito=false&error=correoNoValido");
 die;
 ?>
