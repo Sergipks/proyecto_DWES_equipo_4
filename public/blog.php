ca49daab03401068095245442a9ac37e0876103c
Marcos150 <marcosruizrubio@gmail.com>
2024-03-07 20:25:56 +0100

    Extraída barra de navegación a un fichero y agregados scripts de javascript a algunas páginas que les faltaba.

--- public/blog.php
+++ public/blog.php
@@ -23,30 +23,7 @@
 <body id="page-top">
 
 <!-- Navigation Bar -->
-   <nav class="navbar navbar-fixed-top navbar-default">
-     <div class="container">
-       <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-            <a  class="navbar-brand page-scroll" href="#page-top">
-              <span>[PHOTO]</span>
-            </a>
-       </div>
-       <div class="collapse navbar-collapse navbar-right" id="menu">
-          <ul class="nav navbar-nav">
-            <li class=" lien"><a href="index.html"><i class="fa fa-home sr-icons"></i> Home</a></li>
-            <li class="lien"><a href="about.html"><i class="fa fa-bookmark sr-icons"></i> About</a></li>
-            <li class="active lien"><a href="#"><i class="fa fa-file-text sr-icons"></i> Blog</a></li>
-            <li><a href="contact.html"><i class="fa fa-phone-square sr-icons"></i> Contact</a></li>
-            <li class=" lien"><a href="controllers/especiesController.php"><i class="fa fa-tree sr-icons"></i> Nuestras especies</a></li>
-          </ul>
-       </div>
-     </div>
-   </nav>
+<?php include "views/nav.html" ?>
 <!-- End of Navigation Bar -->
 
 <!-- Principal Content Start -->
